31f277e352ccdb203197601075969274d46995f9
EhsanUllahKhan208 <ehsanullahkhan208@gmail.com>
2024-05-02 19:29:10 +0500

    Method overriding example

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,11 +10,30 @@
         {
             Console.WriteLine(a + b);
         }
+
         static void Main(string[] args)
         {
             Program obj = new Program();
             obj.sum(1,2);
             Console.ReadLine();
+            Dog tommy = new Dog();
+            tommy.eat();
+            Console.ReadLine();
+        }
+    }
+    class Animal
+    {
+        public void eat()
+            {
+            Console.WriteLine("Animal is eating.");
+        }
+
+    }
+    class Dog : Animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Dog is eating.");
         }
     }
 }
