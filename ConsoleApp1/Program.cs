260927e42befe2c2cb8b69cc410125ceea9f64fa
EhsanUllahKhan208 <ehsanullahkhan208@gmail.com>
2024-05-02 19:17:13 +0500

    polymorphism Method overloading example

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace overloading
+{
+     class Program
+    {
+        void sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        void sum(float a,float b)
+        {
+            Console.WriteLine(a + b);
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.sum(1,2);
+            Console.ReadLine();
+        }
+    }
+}
