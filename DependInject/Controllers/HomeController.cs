c51aae9d621f848835cf2229855d3dd49793fb39
EhsanUllahKhan208 <ehsanullahkhan208@gmail.com>
2024-05-05 13:07:12 +0500

    DependencyInjectionSimopleExample

---
+++ DependInject/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using DependInject.Models;
+using DependInject.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DependInject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmailSenderService _service;
+        public HomeController(IEmailSenderService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            _service.SendEmail("Hello World");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DependInject/Services/EmailSenderService.cs
@@ -0,0 +1,14 @@
+﻿namespace DependInject.Services
+{
+    public interface IEmailSenderService
+    {
+        void SendEmail(string email);
+    }
+    public class EmailSenderService : IEmailSenderService
+    {
+        public void SendEmail(string email)
+        {
+            //Todo
+        }
+    }
+}
